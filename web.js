6d79d0782e68799bd984bd8c275f2fc741ec96d5
Cassilds <casilds@hotmail.com>
2013-07-10 11:44:45 +0000

    novamente...

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(index.html);
+var buffer = new Buffer('index.html');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
